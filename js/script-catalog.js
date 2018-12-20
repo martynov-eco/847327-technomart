3af2a4d5a883bc81381518c05d08c7c5337ee238
martynov-eco <martynov.post@gmail.com>
2018-12-20 20:13:14 +0400

    Коммит перед 1 защитой

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -1,7 +1,7 @@
 
 
-var buy = document.querySelectorAll("a.buy-button");
-var addBookmark = document.querySelectorAll("a.bookmarks-button");
+var buys = document.querySelectorAll("a.buy-button");
+var addBookmarks = document.querySelectorAll("a.bookmarks-button");
 var buyModal = document.querySelector(".modal-cart");
 var closeBuy = buyModal.querySelector(".modal-close");
 var cart = document.querySelector(".cart");
@@ -10,41 +10,39 @@ var bookmarks = document.querySelector(".bookmarks");
 var bookmarksCount = document.querySelector(".bookmarks-count");
 var continueBuy = buyModal.querySelector(".button-continue");
 
-
-
-buy.forEach(button => {
-		button.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				buyModal.classList.add("modal-show");
-				cart.classList.add("filled-cart");
-				cartCount.innerHTML = Number(cartCount.innerHTML) + 1;
+for (var i=0; i<buys.length; i++) {
+		buys[i].addEventListener("click", function (evt) {
+			evt.preventDefault();
+			buyModal.classList.add("modal-show");
+			cart.classList.add("filled-cart");
+			cartCount.innerHTML = Number(cartCount.innerHTML) + 1;
 		});
-});	
+};
 
 closeBuy.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		buyModal.classList.remove("modal-show");
+	evt.preventDefault();
+	buyModal.classList.remove("modal-show");
 });
 
 continueBuy.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		buyModal.classList.remove("modal-show");
+	evt.preventDefault();
+	buyModal.classList.remove("modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
-		if (evt.keyCode === 27) {
+	if (evt.keyCode === 27) {
 			evt.preventDefault();
 			if (buyModal.classList.contains("modal-show")) {
-					buyModal.classList.remove("modal-show");
-				}
-		}
+				buyModal.classList.remove("modal-show");
+			}
+	}
 });
 
-addBookmark.forEach(book => {
-		book.addEventListener("click", function (evt) {
-				evt.preventDefault();
+for (var i=0; i<addBookmarks.length; i++) {
+	addBookmarks[i].addEventListener("click", function (evt) {
+		evt.preventDefault();
 				bookmarks.classList.add("filled-bookmarks");
 				bookmarksCount.innerHTML = Number(bookmarksCount.innerHTML) + 1;
-		});
-});	
+	});
+};
 
