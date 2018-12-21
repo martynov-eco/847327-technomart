cf76880d1800acd23f41b300c4cfd1d965a9b282
martynov-eco <martynov.post@gmail.com>
2018-12-21 21:03:53 +0400

    комит перед защитой 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
+var buys = document.querySelectorAll("a.buy-button");
+var addBookmarks = document.querySelectorAll("a.bookmarks-button");
+var buyModal = document.querySelector(".modal-cart");
+var closeBuy = buyModal.querySelector(".modal-close");
+var cart = document.querySelector(".cart");
+var cartCount = document.querySelector(".cart-count");
+var bookmarks = document.querySelector(".bookmarks");
+var bookmarksCount = document.querySelector(".bookmarks-count");
+var continueBuy = buyModal.querySelector(".button-continue");
 
+for (var i=0; i<buys.length; i++) {
+		buys[i].addEventListener("click", function (evt) {
+				evt.preventDefault();
+				buyModal.classList.add("modal-show");
+				cart.classList.add("filled-cart");
+				cartCount.innerHTML = Number(cartCount.innerHTML) + 1;
+		});
+};
+
+closeBuy.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		buyModal.classList.remove("modal-show");
+});
+
+continueBuy.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		buyModal.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+				evt.preventDefault();
+				if (buyModal.classList.contains("modal-show")) {
+						buyModal.classList.remove("modal-show");
+				}
+		}
+});
+
+for (var i=0; i<addBookmarks.length; i++) {
+		addBookmarks[i].addEventListener("click", function (evt) {
+				evt.preventDefault();
+				bookmarks.classList.add("filled-bookmarks");
+				bookmarksCount.innerHTML = Number(bookmarksCount.innerHTML) + 1;
+		});
+};
 
 var feedBack = document.querySelector(".button-write-us");
 var feedBackModal = document.querySelector(".modal-write-us");
@@ -13,7 +57,7 @@ feedBack.addEventListener("click", function (evt) {
 		feedBackModal.classList.add("modal-show");
 		userName.focus();
 });
-	
+
 closeFeedBack.addEventListener("click", function (evt) {
 		evt.preventDefault();
 		feedBackModal.classList.remove("modal-show");
@@ -24,8 +68,8 @@ window.addEventListener("keydown", function (evt) {
 		if (evt.keyCode === 27) {
 				evt.preventDefault();
 				if (feedBackModal.classList.contains("modal-show")) {
-					feedBackModal.classList.remove("modal-show");
-					feedBackModal.classList.remove("modal-error");
+						feedBackModal.classList.remove("modal-show");
+						feedBackModal.classList.remove("modal-error");
 				}
 		}
 });
@@ -38,7 +82,7 @@ form.addEventListener("submit", function (evt) {
 				feedBackModal.classList.add("modal-error");
 		}
 });
-/******************************************* */
+
 var map = document.querySelector(".map a");
 var mapModal = document.querySelector(".modal-map");
 var closeMap = mapModal.querySelector(".modal-close");
@@ -47,7 +91,7 @@ map.addEventListener("click", function (evt) {
 		evt.preventDefault();
 		mapModal.classList.add("modal-show");
 });
-	
+
 closeMap.addEventListener("click", function (evt) {
 		evt.preventDefault();
 		mapModal.classList.remove("modal-show");
@@ -61,50 +105,3 @@ window.addEventListener("keydown", function (evt) {
 				}
 		}
 });
-
-
-var buys = document.querySelectorAll("a.buy-button");
-var addBookmarks = document.querySelectorAll("a.bookmarks-button");
-var buyModal = document.querySelector(".modal-cart");
-var closeBuy = buyModal.querySelector(".modal-close");
-var cart = document.querySelector(".cart");
-var cartCount = document.querySelector(".cart-count");
-var bookmarks = document.querySelector(".bookmarks");
-var bookmarksCount = document.querySelector(".bookmarks-count");
-var continueBuy = buyModal.querySelector(".button-continue");
-
-for (var i=0; i<buys.length; i++) {
-		buys[i].addEventListener("click", function (evt) {
-			evt.preventDefault();
-			buyModal.classList.add("modal-show");
-			cart.classList.add("filled-cart");
-			cartCount.innerHTML = Number(cartCount.innerHTML) + 1;
-		});
-};
-
-closeBuy.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	buyModal.classList.remove("modal-show");
-});
-
-continueBuy.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	buyModal.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-	if (evt.keyCode === 27) {
-			evt.preventDefault();
-			if (buyModal.classList.contains("modal-show")) {
-				buyModal.classList.remove("modal-show");
-			}
-	}
-});
-
-for (var i=0; i<addBookmarks.length; i++) {
-	addBookmarks[i].addEventListener("click", function (evt) {
-		evt.preventDefault();
-				bookmarks.classList.add("filled-bookmarks");
-				bookmarksCount.innerHTML = Number(bookmarksCount.innerHTML) + 1;
-	});
-};
